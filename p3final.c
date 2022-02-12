505e53453f950c0caca48cfc2ff7a235631f415d
Harshit Singh <89565061+Hersheys09@users.noreply.github.com>
2022-02-12 09:42:34 +0000

    p1 ready

---
+++ p3final.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int input_number()
+{
+  int n;
+  printf("Enter a number:\n");
+  scanf("%d", &n);
+  return n;
+}
+
+int is_composite(int n)
+{
+  int c;
+  for(int i=0, c)
+  {
+    uij
+  }
+  return c;
+}
+
+void output(int n, int composite)
+{
+  if(composite = 0)
+    printf("The number %d is Prime\n", n);
+  else
+    printf("The number %d is Composite\n", n);
+}
+
+int main()
+{
+  int n = input_number();
+  int composite = is_composite(n);
+  output(n, composite);
+  return 0;
+}
